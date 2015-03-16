429ddd39b0bdd531cdd8041ade3fcb2f10a9425e
long-long-float <niinikazuki@yahoo.co.jp>
2015-03-16 20:39:44 +0900

    テンプレートを追加

---
+++ templates/c++.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+#define rep(i, n) for(int i = 0;i < (n);i++)
+#define all(c) (c).begin(),(c).end()
+#define min(a, b) ((a) <= (b) ? (a) : (b))
+#define max(a, b) ((a) >= (b) ? (a) : (b))
+
+typedef unsigned int uint;
+
+using namespace std;
+
+int main() {
+  return 0;
+}
