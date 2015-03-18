d22b64c5e2377c6bedfd74e25f90bffdc82a5bc5
long-long-float <niinikazuki@yahoo.co.jp>
2015-03-18 23:31:33 +0900

    min, maxマクロの削除

--- templates/c++.cpp
+++ templates/c++.cpp
@@ -2,8 +2,6 @@
 
 #define rep(i, n) for(int i = 0;i < (n);i++)
 #define all(c) (c).begin(),(c).end()
-#define min(a, b) ((a) <= (b) ? (a) : (b))
-#define max(a, b) ((a) >= (b) ? (a) : (b))
 
 typedef unsigned int uint;
 
